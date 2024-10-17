d300c6f1ac3f26aa29ad24d41a8df51de3d8f4d1
Pedro-H-Medeiros <medeirosprogrammer@gmail.com>
2024-10-17 09:56:04 -0300

    feat: update ong controller

--- src/modules/ong/controllers/update-ong.controller.ts
+++ src/modules/ong/controllers/update-ong.controller.ts
@@ -1,9 +1,14 @@
 import { CurrentUser } from '@/modules/auth/current-user.decorator'
 import { JwtAuthGuard } from '@/modules/auth/jwt-auth.guard'
 import { UserPayload } from '@/modules/auth/jwt.stategy'
-import { ZodValidationPipe } from '@/pipes/zod-validation.pipe'
 import { PrismaService } from '@/prisma/prisma.service'
-import { Body, Controller, Patch, UseGuards, UsePipes } from '@nestjs/common'
+import {
+  Body,
+  ConflictException,
+  Controller,
+  Patch,
+  UseGuards,
+} from '@nestjs/common'
 import { z } from 'zod'
 
 const updateOngControllerBodySchema = z.object({
@@ -20,16 +25,32 @@ export class UpdateOngController {
   constructor(private prisma: PrismaService) {}
 
   @Patch()
-  @UsePipes(new ZodValidationPipe(updateOngControllerBodySchema))
+  // @UsePipes(new ZodValidationPipe(updateOngControllerBodySchema))
   async update(
     @CurrentUser() user: UserPayload,
     @Body() body: UpdateOngControllerBodySchema,
   ) {
     const { name } = body
 
-    await this.prisma.ong.update({
+    const ongExists = await this.prisma.ong.findFirst({
+      where: {
+        name,
+      },
+    })
+
+    if (ongExists) {
+      throw new ConflictException('This ong name already exists.')
+    }
+
+    const userAccount = await this.prisma.user.findUnique({
       where: {
         id: user.sub,
+      },
+    })
+
+    return await this.prisma.ong.update({
+      where: {
+        id: userAccount.ong_id,
       },
       data: {
         name,
