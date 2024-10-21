a466dc5b8c05475c439541679b74d57cc933abd1
Pedro-H-Medeiros <medeirosprogrammer@gmail.com>
2024-10-21 08:39:47 -0300

    feat: rbac authorization application

--- src/modules/ong/controllers/update-ong.controller.ts
+++ src/modules/ong/controllers/update-ong.controller.ts
@@ -2,6 +2,9 @@ import { MissingFieldException } from '@/exceptions/missing-field.exception'
 import { CurrentUser } from '@/modules/auth/current-user.decorator'
 import { JwtAuthGuard } from '@/modules/auth/jwt-auth.guard'
 import { UserPayload } from '@/modules/auth/jwt.stategy'
+import { RolesGuard } from '@/modules/user/enum/roles.guard'
+import { Role } from '@/modules/user/enum/user-roles.enum'
+import { Roles } from '@/modules/user/roles.decorator'
 import { ZodValidationPipe } from '@/pipes/zod-validation.pipe'
 import { PrismaService } from '@/prisma/prisma.service'
 import {
@@ -23,11 +26,13 @@ type UpdateOngControllerBodySchema = z.infer<
 >
 
 @Controller('instituation')
+@UseGuards(RolesGuard)
 @UseGuards(JwtAuthGuard)
 export class UpdateOngController {
   constructor(private prisma: PrismaService) {}
 
   @Patch()
+  @Roles(Role.OngAdmin && Role.Admin)
   @UsePipes(new ZodValidationPipe(updateOngControllerBodySchema))
   async update(
     @CurrentUser() user: UserPayload,
