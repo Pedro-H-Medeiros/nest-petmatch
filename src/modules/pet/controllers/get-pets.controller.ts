5a6c4b357500ff26694a355fb71659272cd1f5e5
Pedro-H-Medeiros <medeirosprogrammer@gmail.com>
2024-12-16 20:37:41 -0300

    feat: filter pet

--- src/modules/pet/controllers/get-pets.controller.ts
+++ src/modules/pet/controllers/get-pets.controller.ts
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { PrismaService } from '@/prisma/prisma.service'
 import { Controller, Get, Query } from '@nestjs/common'
+import { subMonths, subYears } from 'date-fns'
 
 @Controller('pets')
 export class GetPetsController {
@@ -8,33 +9,11 @@ export class GetPetsController {
 
   @Get()
   async getPets(
-    @Query('maxYears') maxYears?: string,
-    @Query('maxMonths') maxMonths?: string,
-    @Query('minYears') minYears?: string,
+    @Query('age') age?: string,
     @Query('sex') sex?: string,
     @Query('type') type?: string,
   ) {
     const filters: Record<string, any> = {}
-
-    const now = new Date()
-
-    if (maxYears || maxMonths) {
-      const maxDate = new Date(now)
-      if (maxYears)
-        maxDate.setFullYear(now.getFullYear() - parseInt(maxYears, 10))
-      if (maxMonths) maxDate.setMonth(now.getMonth() - parseInt(maxMonths, 10))
-
-      filters.age = { gte: maxDate }
-    }
-
-    if (minYears) {
-      const minDate = new Date(now)
-      minDate.setFullYear(now.getFullYear() - parseInt(minYears, 10))
-
-      filters.age = filters.age
-        ? { ...filters.age, lt: minDate }
-        : { lt: minDate }
-    }
 
     if (sex) {
       filters.sex = sex
@@ -44,10 +23,40 @@ export class GetPetsController {
       filters.type = type
     }
 
+    if (age) {
+      const ageFilter = this.getAgeFilter(age)
+
+      if (ageFilter) {
+        filters.age = ageFilter
+      }
+    }
+
     const pets = await this.prisma.pet.findMany({
       where: filters,
     })
 
     return { pets }
+  }
+
+  /**
+   * Retorna os filtros de data baseados na idade fornecida.
+   * @param age - Filtro de idade ('less_than_six_months', 'up_to_five_years', etc.)
+   * @returns Objeto com condições para o campo `created_at`, ou `null` se a idade for inválida.
+   */
+  private getAgeFilter(age: string) {
+    const now = new Date()
+
+    switch (age) {
+      case 'less_than_six_months':
+        return { gte: subMonths(now, 6) }
+      case 'up_to_five_years':
+        return { gte: subYears(now, 5) }
+      case 'up_to_ten_years':
+        return { gte: subYears(now, 10) }
+      case 'more_than_ten_years':
+        return { lt: subYears(now, 10) }
+      default:
+        return null
+    }
   }
 }
